4064d7e0ae98673faa69023e85f1ce1e892f7691
Howard <howardshen2306@gmail.com>
2022-09-10 02:01:57 +0800

    feat: create server and socket.io connected

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const cors = require('cors');
+const { API_VERSION, PORT } = process.env;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+// API routes
+app.use('/api/' + API_VERSION, [
+  require('./server/routes/upload_images'),
+  require('./server/routes/user'),
+]);
+
+// socket.io
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected ${socket.id}`);
+
+  socket.on('sendMessage', (data) => {
+    console.log('front-end sent data', data);
+
+    io.emit('receiveMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on *:${PORT}`);
+});
