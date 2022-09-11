28840eb35c079f9ba5dc013506a201d07e37b89c
Howard <howardshen2306@gmail.com>
2022-09-11 15:13:53 +0800

    refactor: modify code layout

--- server/models/user.js
+++ server/models/user.js
@@ -4,29 +4,25 @@ const { User } = require('../models/schema');
 const signUp = async (name, email, password) => {
   try {
     const user = await User.create({
-      name: name,
-      email: email,
-      password: password,
+      name,
+      email,
+      password,
     });
     console.log(user);
     return 'Register Success';
   } catch (error) {
     console.log('error', error.message);
-    return { error: 'Email Already Exists', status: 403 };
+    return { error: 'Email Already Exists' };
   }
 };
 
-const signIn = async (email, password) => {
+const signIn = async (email) => {
   try {
-    const user = await User.findOne({ email: email }).exec();
-    if (user.email !== null) {
-      return 'Login success';
-    } else {
-      return 'Email not find';
-    }
+    const user = await User.findOne({ email }).exec();
+    return user;
   } catch (error) {
-    console.log(error.message);
-    return error.message;
+    console.log('error:', error.message);
+    return { error: error.message };
   }
 };
 
