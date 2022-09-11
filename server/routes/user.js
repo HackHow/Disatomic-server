bc8becf40f823ce2f1a9544b30e343a5f3f8fe68
Howard <howardshen2306@gmail.com>
2022-09-11 12:09:26 +0800

    feat: add user profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { signUp, signIn } = require('../controllers/user');
+const { signUp, signIn, profile } = require('../controllers/user');
 
 router.route('/user/signup').post(signUp);
 router.route('/user/signin').post(signIn);
+router.route('/user/profile').get(profile);
 
 module.exports = router;
