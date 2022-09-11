f2c0823c917a6ca2e9f37a060a1ca68e8e1ce20b
Howard <howardshen2306@gmail.com>
2022-09-11 14:01:47 +0800

    feat: add jwt token sign

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const User = require('../models/user');
+const { SECRET, EXPIRED } = process.env;
+const { jwtSign } = require('../../utils/util');
 
 const signUp = async (req, res) => {
   const { name, email, password } = req.body;
-  // console.log('data:', [name, email, password]);
   const result = await User.signUp(name, email, password);
-  res.send(result);
+
+  if (result.error) {
+    res.status(403).send({ error: result.error });
+    return;
+  }
+
+  const jwtToken = await jwtSign({ name, email, password }, SECRET, EXPIRED);
+
+  res.status(200).send({ accessToken: jwtToken, expired: EXPIRED });
 };
 
 const signIn = async (req, res) => {
