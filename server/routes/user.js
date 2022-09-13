007237310ee23ac7268623d17a5a916bad02fcfb
Howard <howardshen2306@gmail.com>
2022-09-13 16:53:31 +0800

    feat: add reject friend invitation api

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,8 @@ const {
   profile,
   sendFriendInvitation,
   acceptFriend,
+  rejectFriend,
+  //   cancelFriend,
 } = require('../controllers/user');
 
 router.route('/user/signup').post(signUp);
@@ -12,7 +14,10 @@ router.route('/user/signin').post(signIn);
 router.route('/user/profile').get(profile);
 
 router.route('/user/friend').post(sendFriendInvitation);
-router.route('/user/friend/pending').post(acceptFriend);
+// router.route('/user/friend/pending').get();
+router.route('/user/friend/accept').post(acceptFriend);
+router.route('/user/friend/reject').post(rejectFriend);
+// router.route('/user/friend/cancel').post(cancelFriend);
 router.route('/user/friend/:state').get();
 
 module.exports = router;
