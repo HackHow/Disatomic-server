7d15fd3b2c3f9b2ed5bf8f1affd5a384e3b0b53d
Howard <howardshen2306@gmail.com>
2022-09-18 16:05:21 +0800

    feat: move friend series code

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,24 +1,9 @@
 const router = require('express').Router();
-const {
-  signUp,
-  signIn,
-  userInfo,
-  // sendFriendInvitation,
-  // acceptFriend,
-  // rejectFriend,
-  // cancelFriend,
-} = require('../controllers/user');
+const { signUp, signIn, userInfo } = require('../controllers/user');
 const { authentication } = require('../../utils/util');
 
 router.route('/user/signup').post(signUp);
 router.route('/user/signin').post(signIn);
 router.route('/user').get(authentication, userInfo);
-
-// router.route('/user/friend').post(sendFriendInvitation);
-// router.route('/user/friend/pending').get();
-// router.route('/user/friend/accept').post(acceptFriend);
-// router.route('/user/friend/reject').post(rejectFriend);
-// router.route('/user/friend/cancel').post(cancelFriend);
-// router.route('/user/friend/:state').get();
 
 module.exports = router;
